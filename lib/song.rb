90e1f32ce04b6fb4e6f318d693431b784b0d9264
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-08-21 08:37:58 +0300

    commit tree d4c0a4dd11759bebe1eb3a6362d0ff257b7370fd

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song 
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ninety_nine_problems.name
+ninety_nine_problems.artist
+ninety_nine_problems.genre
